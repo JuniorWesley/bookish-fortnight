e2c9b737c53da1454ffe3657e2e52412e4e5cc5a
JuniorWesley <wesley.junior@aptiv.com>
2022-04-18 09:29:35 -0300

    :sparkles: feat: create right kanban card

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let windowObjectReference;
 const windowFeatures = "popup";
 
 function OpenPopUp() {
-  windowObjectReference = window.open("http://localhost/sam/frontend/view/login/index.php", "Imprimir cartões", windowFeatures);
+  windowObjectReference = window.open("cartao_circuito.php", "Imprimir cartões", windowFeatures);
 }
 
 const button = document.querySelector("button")
